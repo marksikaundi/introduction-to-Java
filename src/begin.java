4f882cdb4317017fa0996c2543060f0812791f9a
Mark Sikaundi <sikaundimark@gmail.com>
2021-10-20 15:41:29 +0200

    On branch master

---
+++ src/begin.java
@@ -0,0 +1,13 @@
+
+public class begin {
+
+	public static void main(String[] args) {
+		/* java syntax
+		 java is case-Senstive
+		 THE CURLY BRACES {} marks the beginning and the ending of a block of code
+		 each code statement must end with a semicolon */
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
---
+++ src/variables.java
@@ -0,0 +1,42 @@
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* variables are containers for storing data values. in java, there are different types of variables, for example:
+		 * string-store text
+		 * int-stores integers
+		 * float-stores floating point numbers
+		 * char-stores single characters
+		 * boolean-stores values with two states
+		
+		 *  */
+		String name = "Mark"; //THIS CODE WILL CREATE A STRING
+		System.out.println(name);
+
+		int myNum = 5;
+		System.out.println(myNum);
+
+		float myFloatNum = 5.99f;
+		System.out.println(myFloatNum);
+
+		char myLetter = 'D';
+		System.out.println(myLetter);
+
+		boolean myBool = true;
+		System.out.println(myBool);
+
+		String myText = "Hello";
+		System.out.println(myText);
+
+		String Name = "Mark";
+		System.out.println("Hello " + Name);
+		
+		String firstName = "Mark ";
+		String lastName = "Sikaundi";
+		String fullName = firstName + lastName;
+		System.out.println(fullName);
+		
+	}
+
+}
